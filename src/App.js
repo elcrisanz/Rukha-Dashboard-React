583b5c42e90dfbcbc02b087d9bc869a53203dcce
Cristian Sánchez <cristian.sanz@gmail.com>
2021-10-26 21:18:09 -0300

    Primera subida Dashboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import Topbar from './components/Topbar';
+import Amountproducts from './components/Amountproducts';
+import Totalproducts from './components/Totalproducts';
+import Totalusers from './components/Totalusers';
+import Lastproduct from './components/Lastproduct';
+import Categories from './components/Categories';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="wrapper">
+      <Sidebar />
+      <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+          <Topbar />
+          <div class="container-fluid">				
+					  <div class="d-sm-flex align-items-center justify-content-between mb-4">
+						  <h1 class="h3 mb-0 text-gray-800">App Dashboard</h1>
+					  </div>
+            <div class="row">
+              <Totalproducts />
+              <Amountproducts />
+              <Totalusers />
+            </div>
+            <div class="row">
+              <Lastproduct />
+              <Categories />
+            </div>
+          </div>
+        </div>
+        <footer class="sticky-footer bg-white">
+				<div class="container my-auto">
+					<div class="copyright text-center my-auto">
+						<span>Copyright &copy; Dashboard 2020</span>
+					</div>
+				</div>
+			</footer>
+      </div>  
+    </div>  
   );
 }
 
